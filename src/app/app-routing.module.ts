6f6cb93af1134312810e6323925420ec5c291261
fimovol <fimovolpe@gmail.com>
2023-01-20 16:09:08 -0500

    añadiendo canactivate guard para el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,16 +8,21 @@ import { Reto19Component } from './principal/reto19/reto19.component';
 import { Reto20Component } from './principal/reto20/reto20.component';
 import { RetoComponent } from './principal/reto/reto.component';
 import { VistaComponent } from './principal/vista/vista.component';
+import { CanactivateGuard } from './canactivate.guard';
+import { LoginComponent } from './principal/login/login.component';
 
 const routes: Routes = [
-  {path:"vista",component:VistaComponent},
-  {path:"busqueda",component:BusquedaComponent},
-  {path:"form",component:FormularioComponent},
-  {path:"reto19",component:Reto19Component},
-  {path:"reto20",component:Reto20Component},
-  {path:"reto",component:RetoComponent},
-  {path:"asin20",component:Asincrona20Component},
-  {path:"asincrona19",component:AsincronaComponent},
+  {path:"vista",component:VistaComponent,canActivate: [CanactivateGuard]},
+  {path:"busqueda",component:BusquedaComponent,canActivate: [CanactivateGuard]},
+  {path:"form",component:FormularioComponent,canActivate: [CanactivateGuard]},
+  {path:"reto19",component:Reto19Component,canActivate: [CanactivateGuard]},
+  {path:"reto20",component:Reto20Component,canActivate: [CanactivateGuard]},
+  {path:"reto",component:RetoComponent,canActivate: [CanactivateGuard]},
+  {path:"asin20",component:Asincrona20Component,canActivate: [CanactivateGuard]},
+  {path:"asincrona19",component:AsincronaComponent,canActivate: [CanactivateGuard]},
+  {path:"login",component:LoginComponent},
+  {path:"",component:LoginComponent},
+  {path:"**",component:LoginComponent},
 ];
 
 @NgModule({
