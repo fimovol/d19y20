a009b29cbb1d0a5b93549a9aae739dfb44ac35d2
fimovol <fimovolpe@gmail.com>
2023-01-23 18:01:26 -0500

    validaciones en el registro

--- src/app/principal/retoformregistro/retoformregistro.component.ts
+++ src/app/principal/retoformregistro/retoformregistro.component.ts
@@ -13,18 +13,47 @@ export class RetoformregistroComponent implements OnInit {
   apellido = ""
   user = ""
   pass = ""
+  nombreValid: boolean | undefined;
+  apellidoValid: boolean | undefined;
+  userValid: boolean | undefined;
+  passValid: boolean | undefined;
   enviado = false
   ngOnInit(): void {
   } 
-  onSubmit(){
-    alert("mensaje de "+this.nombre+" "+this.apellido)
-    this.enviado=true
-    let datos = {
-      nombre:this.nombre,
-      apellido:this.apellido,
-      user: this.user,
+  validaciones() {
+    if (this.nombre.length > 0) {
+      this.nombreValid = true;
+    } else {
+      this.nombreValid = false;
     }
-    localStorage.setItem("datos",JSON.stringify(datos))
+    if (this.apellido.length > 0) {
+      this.apellidoValid = true;
+    } else {
+      this.apellidoValid = false;
+    }
+    if (this.user.length > 0) {
+      this.userValid = true;
+    } else {
+      this.userValid = false;
+    }
+    if (this.pass.length > 0) {
+      this.passValid = true;
+    } else {
+      this.passValid = false;
+    }
+  }
+  onSubmit(){
+    this.validaciones();
+    if(this.nombreValid && this.apellidoValid && this.userValid && this.passValid){
+      alert("mensaje de "+this.nombre+" "+this.apellido)
+      this.enviado=true
+      let datos = {
+        nombre:this.nombre,
+        apellido:this.apellido,
+        user: this.user,
+      }
+      localStorage.setItem("datos",JSON.stringify(datos))
+    }
   }
   salirDeRuta():Observable<boolean> | boolean{
     if(!this.nombre && !this.apellido && !this.user && !this.pass || this.enviado){
